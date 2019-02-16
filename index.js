56a0e098fc6596327c8582f7a5e27e8018ca4f14
Steveraaen <sraaen@gmail.com>
2019-02-16 14:44:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+
+require('dotenv').config()
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+
+
+var pool  = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: '3306',
+    user: 'tranch5_sjr',
+    password: process.env.DB_PW,
+    database: 'tranch5_milb'
+});
+app.get('/api/summary', function(req, res) {
+	pool.getConnection(function(err, connection) {
+  connection.query('select year, minorTeam,  count(player), franchise, class from supermaster  group by minorTeam , year order by count(player) desc', function (error, results, fields) {
+    console.log(results)
+    	res.json(results)
+    connection.release();
+
+    if (error) throw error;
+   });
+ });
+})
+app.get('/api/allMinors', function(req, res) {
+	pool.getConnection(function(err, connection) {
+  connection.query('select * from newMinors where franchise IS NOT NULL', function (error, results, fields) {
+    console.log(results)
+    	res.json(results)
+    connection.release();
+
+    if (error) throw error;
+   });
+ });
+})
+app.get('/api/bestMinors', function(req, res) {
+  console.log(req.query)
+  pool.getConnection(function(err, connection) {
+  connection.query(`select newMinors.team, count(distinct newPlayerMaster.playerID) as playerCount, newPlayerMaster.franchise
+from newPlayerMaster, newMinors
+where newPlayerMaster.classes REGEXP ? 
+and newMinors.class = ?
+and newMinors.franchise = newPlayerMaster.franchise
+and newPlayerMaster.yr = ?
+group by newMinors.team
+order by count(newPlayerMaster.playerName) desc`, [req.query.m, req.query.p, 2013], function (error, results, fields) {
+    console.log(results)
+      res.json(results)
+    connection.release();
+
+    if (error) throw error;
+   });
+ });
+})
+
+
+
+const port = process.env.PORT || 5001;
+app.listen(port);
+console.log(`Listening on ${port}`);
