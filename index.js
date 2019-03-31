b95371565f70d19034ca70d8e8e1708de11e79ef
Steveraaen <sraaen@gmail.com>
2019-03-31 17:18:01 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const app = express()
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
+/*  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+}*/
 
 console.log(process.env)
 var pool  = mysql.createPool({
