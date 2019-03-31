1c41ec5e18e302af7b0b8a27f9c97a3a7f4ed942
Steveraaen <sraaen@gmail.com>
2019-03-31 14:02:21 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const request = require('request')
 const app = express()
 
 
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/index.html'));
-});
 console.log(__dirname)
 var pool  = mysql.createPool({
     host: process.env.DB_HOST,
