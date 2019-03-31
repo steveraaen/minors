ea77b534a19ba098f8c2a61a29d5daecda7d9cd3
Steveraaen <sraaen@gmail.com>
2019-03-31 17:12:49 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
 require('dotenv').config()
 const express = require('express')
-const path = require('path');
+/*const path = require('path');*/
 const mysql = require('mysql')
 const cheerio = require('cheerio')
 const request = require('request')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-console.log(__dirname)
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+console.log(process.env)
 var pool  = mysql.createPool({
     host: process.env.DB_HOST,
     port: '3306',
